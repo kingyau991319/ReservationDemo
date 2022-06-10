42ef9dcd815874da985dba83d9ccd25af6049f3d
kingyau991319 <kingyau991319@yahoo.com.hk>
2022-06-10 17:25:13 +0800

    code format

--- src/components/NewResv/Form/PersonInfo/TextInputElement/Phone.tsx
+++ src/components/NewResv/Form/PersonInfo/TextInputElement/Phone.tsx
@@ -7,7 +7,6 @@ import TextField from '@mui/material/TextField';
 // i18n
 import { useTranslation } from 'react-i18next';
 
-
 const Phone = (props : {value:string, setPhone:Function}) => {
   const { t } = useTranslation();
   const { setPhone } = props;
